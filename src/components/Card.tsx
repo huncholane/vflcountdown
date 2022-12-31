b92c45a9df16b7b801da220e3b9e43669b8b1cc8
FIRST_NAME LAST_NAME <huncho@ghostsytems.io>
2022-12-31 08:00:49 -0800

    moving to new div

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,25 +1,34 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import TimeBlock from "./TimeBlock";
+import { schedule } from "../assets/schedule.json";
+const { games } = schedule;
 
 type Props = {
   children?: React.ReactNode;
 };
 
 const Card = (props: Props) => {
-  const days = useRef(0);
-  const hours = useRef(1);
-  const mins = useRef(2);
-  const secs = useRef(3);
+  console.log(games);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const nextGame = () => {
+    return games[0];
+  };
+
+  const game = nextGame();
+
   return (
     <div className="h-screen w-screen relative">
-      <div className="absolute mx-auto w-11/12 h-28 right-0 left-0 bg-slate-400 top-16 opacity-40 rounded-md shadow-sm"></div>
+      <div className="absolute mx-auto w-11/12 h-56 right-0 left-0 bg-slate-400 top-16 opacity-40 rounded-md shadow-sm"></div>
       <div className="absolute mx-auto w-11/12 h-28 right-0 left-0 top-16 flex px-1">
-        <TimeBlock ref={days} unit="DAYS" />
-        <TimeBlock ref={hours} unit="HOURS" />
-        <TimeBlock ref={mins} unit="MINS" />
-        <TimeBlock ref={secs} unit="SECS" />
+        <TimeBlock value={days} unit="DAYS" />
+        <TimeBlock value={hours} unit="HOURS" />
+        <TimeBlock value={minutes} unit="MINS" />
+        <TimeBlock value={minutes} unit="SECS" />
       </div>
-      ;
     </div>
   );
 };
--- src/components/TimeBlock.tsx
+++ src/components/TimeBlock.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 type Props = {
-  ref: React.MutableRefObject<number>;
   unit: string;
+  value: number;
 };
 
 const TimeBlock = (props: Props) => {
   return (
     <div className="w-full my-2 mx-1 bg-slate-400 opacity-90 text-center flex flex-col justify-center rounded-sm">
-      <div className="text-orange-500 font-mono font-bold text-4xl">{0}</div>
+      <div className="text-orange-500 font-mono font-bold text-4xl">
+        {props.value}
+      </div>
       <div className="text-white font-mono text-xl">{props.unit}</div>
     </div>
   );
