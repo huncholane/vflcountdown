2f7c8df235bc6b0fcb3ecabb871d164f0b863ab0
FIRST_NAME LAST_NAME <huncho@ghostsytems.io>
2022-12-31 12:31:13 -0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-source /home/ubuntu/.bashrc
-nvm use 18.2.0
-echo `sudo node -v`
-echo $HOME
-echo `/home/ubuntu/.nvm/versions/node/v18.2.0/bin/node -v`
-echo `node -v`
+
+npm install
+npm run build
